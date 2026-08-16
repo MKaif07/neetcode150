91d674904b5c6c8a4d3bc964ea1c99617f940810
kaif2002mk@gmail.com <kaif2002mk@gmail.com>
2026-08-16 22:37:06 +0530

    added 2 sum

--- Array & Hashing/guide.md
+++ Array & Hashing/guide.md
@@ -1,2 +1,3 @@
 Contains Duplicate: https://neetcode.io/problems/duplicate-integer/question
 Valid Anagram: https://neetcode.io/problems/is-anagram/question
+Two Sum: https://neetcode.io/problems/two-integer-sum/question
---
+++ Array & Hashing/twoSum.py
@@ -0,0 +1,26 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        A = []
+        for i, num in enumerate(nums):
+            A.append([num, i])
+
+        A.sort()
+        i,j = 0, len(nums)-1
+        while(i<j):
+            cur = A[i][0] + A[j][0]
+            if cur == target:
+                return [min(A[i][1], A[j][1]), max(A[i][1], A[j][1])]
+                # return [A[i][1], A[j][1]]
+            elif cur < target:
+                i += 1
+            else:
+                j -= 1
+        
+        return []
+            
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [-1,-2,-3,-4,-5]
+    target = -8
+    print(sol.twoSum(nums, target))
