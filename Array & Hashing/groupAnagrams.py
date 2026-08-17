51f64a610c244b3aed3b37fabe4ccbc4f996df41
kaif2002mk@gmail.com <kaif2002mk@gmail.com>
2026-08-17 17:49:19 +0530

    Group Anagrams

---
+++ Array & Hashing/groupAnagrams.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = "".join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
+            
+if __name__ == "__main__":
+    sol = Solution()
+    strs = ["act","pots","tops","cat","stop","hat"]
+    print(sol.groupAnagrams(strs))
--- Array & Hashing/guide.md
+++ Array & Hashing/guide.md
@@ -1,3 +1,4 @@
 Contains Duplicate: https://neetcode.io/problems/duplicate-integer/question
 Valid Anagram: https://neetcode.io/problems/is-anagram/question
 Two Sum: https://neetcode.io/problems/two-integer-sum/question
+Group Anagrams: https://neetcode.io/problems/anagram-groups/question
